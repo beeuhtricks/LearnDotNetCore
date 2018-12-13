949c67684768db110402c43037b2deb2f79ce092
Beatrix Klebe <beatrix@qnect.com>
2018-12-12 20:09:35 -0500

    Write tests for extension methods

---
+++ LearnCSharpTests/Library/ExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿namespace LearnCSharpTests.Library
+{
+    using System;
+    using System.IO;
+
+    using LearnFSharpLibrary;
+
+    using Xunit;
+
+    public class ExtensionsTests
+    {
+        [Fact]
+        public void PrintStringEqual()
+        {
+            var consoleOutput = new StringWriter();
+            Console.SetOut(consoleOutput);
+            const string TestString = "This is a test.";
+            TestString.Print();
+            Assert.Equal(TestString + Environment.NewLine, consoleOutput.ToString());
+        }
+    }
+}
---
+++ LearnFSharpTests/Library/ExtensionsTests.fs
@@ -0,0 +1,14 @@
+﻿module ExtensionsTests
+
+open System
+open System.IO
+open Xunit
+open LearnFSharpLibrary.Extensions
+
+[<Fact>]
+let PrintTest() =
+    let consoleOutput = new StringWriter()
+    Console.SetOut consoleOutput
+    let testString = "This is a test."
+    testString.Print()
+    Assert.Equal(testString + Environment.NewLine, consoleOutput.ToString())
