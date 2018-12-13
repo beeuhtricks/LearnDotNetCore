256d75bfe122cc4ef80de5d5ca8b48dd886620c6
Beatrix Klebe <beatrix@qnect.com>
2018-12-12 20:10:09 -0500

    Remove unnecessary module open

--- LearnFSharpLibrary/Extensions.fs
+++ LearnFSharpLibrary/Extensions.fs
@@ -2,9 +2,11 @@ namespace LearnFSharpLibrary
 
 open System
 open System.Runtime.CompilerServices
-open System.Reflection
 
 [<Extension>]
 module Extensions =
     [<Extension>]
     let Print(s : string) = printfn "%s" s
+
+    type String with
+        member s.Print() = printfn "%s" s
