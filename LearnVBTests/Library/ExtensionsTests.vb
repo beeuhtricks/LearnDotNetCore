1c7499748e0ae28fc93e1930e9c528d246463c2f
Beatrix Klebe <beatrix@qnect.com>
2018-12-12 20:17:09 -0500

    Test VB string Print extension method

---
+++ LearnVBTests/Library/ExtensionsTests.vb
@@ -0,0 +1,16 @@
+﻿Imports System.IO
+Imports LearnVBLibrary
+Imports Xunit
+
+Public Class ExtensionsTests
+    <Fact>
+    Sub PrintStringEqual()
+        Dim consoleOutput = New StringWriter
+        Console.SetOut(consoleOutput)
+
+        Const testString = "This is a test."
+        testString.Print()
+
+        Assert.Equal($"This is a test.{Environment.NewLine}", consoleOutput.ToString())
+    End Sub
+End Class
